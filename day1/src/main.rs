53f8b777faa9579ac5507f7c6c7f18c0412738f3
Adam Chalmers <adamschalmers@gmail.com>
2021-12-02 09:38:01 -0600

    Dramatically simplified. I realized that the last two windows will never be increased compared to the third-last window, so there's no need to handle them. This means I can just use the .windows method

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,13 +1,11 @@
-use std::collections::VecDeque;
-
 type Depth = u16;
 
 fn main() {
     let depths = read(include_str!("input.txt"));
-    let part1 = count_increases(depths.clone().into_iter());
+    let part1 = count_increases(depths.iter());
     println!("Part 1 answer: {}", part1);
 
-    let window_depths = Window::new(depths, 3).unwrap();
+    let window_depths = depths.windows(3).map(|w| w.iter().sum::<u16>());
     let part2 = count_increases(window_depths);
     println!("Part 2 answer: {}", part2);
 }
@@ -25,73 +23,14 @@ fn count_increases<T: Ord, Iter: Iterator<Item = T>>(mut iter: Iter) -> u16 {
     increases
 }
 
-/// Scans over the vector with a window of the given length.
-/// When there are no more elements left, it will yield smaller and smaller windows.
-struct Window {
-    curr: VecDeque<Depth>,
-    remaining: Vec<Depth>,
-    max_length: usize,
-}
-
-impl Window {
-    fn new(mut vec: Vec<Depth>, max_length: usize) -> Option<Self> {
-        vec.reverse(); // This way, pop removes items from the start
-        let mut curr = VecDeque::new();
-        for _ in 1..max_length {
-            curr.push_front(vec.pop()?);
-        }
-        Some(Self {
-            curr,
-            remaining: vec,
-            max_length,
-        })
-    }
-}
-
-impl Iterator for Window {
-    type Item = Depth;
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.remaining.pop() {
-            Some(next) => {
-                // Make sure the window never exceeds the maximum length.
-                if self.curr.len() == self.max_length {
-                    self.curr.pop_back()?;
-                }
-                self.curr.push_front(next);
-
-                Some(self.curr.iter().sum())
-            }
-            None => {
-                // Shrink the window, sum any items left in it.
-                self.curr.pop_back()?;
-                if self.curr.is_empty() {
-                    None
-                } else {
-                    Some(self.curr.iter().sum())
-                }
-            }
-        }
-    }
-}
-
 /// Read a newline-separated list of Depths.
 fn read(file: &str) -> Vec<Depth> {
-    file.split('\n').map(|s| s.parse().unwrap()).collect()
+    file.lines().map(|s| s.parse().unwrap()).collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_window() {
-        let mut window = Window::new(vec![1, 2, 3, 4], 2).unwrap();
-        assert_eq!(window.next(), Some(3));
-        assert_eq!(window.next(), Some(5));
-        assert_eq!(window.next(), Some(7));
-        assert_eq!(window.next(), Some(4));
-        assert_eq!(window.next(), None);
-    }
 
     #[test]
     fn test_part_1() {
@@ -102,7 +41,7 @@ mod tests {
     #[test]
     fn test_part_2() {
         let depths_example = read(include_str!("example.txt"));
-        let depths_example_window = Window::new(depths_example, 3).unwrap();
+        let depths_example_window = depths_example.windows(3).map(|w| w.iter().sum::<Depth>());
         assert_eq!(count_increases(depths_example_window), 5);
     }
 }
