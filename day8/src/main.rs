23ae2f1d73200e5b5c5866c038c3df9b3a2d5d64
Adam Chalmers <adamschalmers@gmail.com>
2021-12-08 17:23:28 -0600

    Day 8 part 1

---
+++ day8/src/main.rs
@@ -0,0 +1,38 @@
+use parse::Observation;
+use std::collections::HashSet;
+
+mod parse;
+fn main() {
+    let observations = Observation::parse_lines(include_str!("input.txt"))
+        .unwrap()
+        .1;
+    let q1 = count_unique_len(&observations);
+    println!("Q1: {}", q1);
+}
+
+fn count_unique_len(observations: &[Observation]) -> usize {
+    let unique_len: HashSet<_> = HashSet::from([2, 3, 4, 7]);
+    observations
+        .iter()
+        .map(|p| {
+            p.output
+                .iter()
+                .filter(|segs| unique_len.contains(&segs.len()))
+                .count()
+        })
+        .sum()
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_count_unique_len() {
+        let observations = Observation::parse_lines(include_str!("example.txt"))
+            .unwrap()
+            .1;
+        let actual = count_unique_len(&observations);
+        let expected = 26;
+        assert_eq!(actual, expected);
+    }
+}
---
+++ day8/src/parse.rs
@@ -0,0 +1,113 @@
+use nom::{
+    bytes::complete::{tag, take_while},
+    combinator::map_res,
+    multi::separated_list1,
+    sequence::separated_pair,
+    IResult,
+};
+use std::collections::HashSet;
+use std::convert::TryInto;
+
+#[derive(Hash, Eq, PartialEq, Ord, PartialOrd, Debug)]
+pub enum Segment {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+}
+
+impl TryFrom<char> for Segment {
+    type Error = &'static str;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        let seg = match value {
+            'a' => Self::A,
+            'b' => Self::B,
+            'c' => Self::C,
+            'd' => Self::D,
+            'e' => Self::E,
+            'f' => Self::F,
+            'g' => Self::G,
+            _ => return Err("not a segment"),
+        };
+        Ok(seg)
+    }
+}
+
+fn is_segment(c: char) -> bool {
+    Segment::try_from(c).is_ok()
+}
+
+fn to_segs(s: &str) -> Result<HashSet<Segment>, &'static str> {
+    if s.is_empty() {
+        return Err("cannot have empty set of segments");
+    }
+    let mut set = HashSet::default();
+    for c in s.chars() {
+        set.insert(Segment::try_from(c)?);
+    }
+    Ok(set)
+}
+
+pub struct Observation {
+    patterns: [HashSet<Segment>; 10],
+    pub output: [HashSet<Segment>; 4],
+}
+
+impl Observation {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        let (input, (patterns, output)) =
+            separated_pair(patterns_parser, tag(" | "), patterns_parser)(input)?;
+        let patterns = patterns.try_into().unwrap();
+        let output = output.try_into().unwrap();
+        Ok((input, Self { patterns, output }))
+    }
+    pub fn parse_lines(input: &str) -> IResult<&str, Vec<Self>> {
+        separated_list1(tag("\n"), Observation::parse)(input)
+    }
+}
+
+fn patterns_parser(input: &str) -> IResult<&str, Vec<HashSet<Segment>>> {
+    separated_list1(tag(" "), segments_parser)(input)
+}
+
+fn segments_parser(input: &str) -> IResult<&str, HashSet<Segment>> {
+    map_res(take_while(is_segment), to_segs)(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_segments() {
+        let actual = segments_parser("ab").unwrap().1;
+        assert_eq!(actual, HashSet::from([Segment::A, Segment::B]))
+    }
+
+    #[test]
+    fn parse_patterns() {
+        let actual = patterns_parser("ab def ").unwrap().1;
+        assert_eq!(
+            actual,
+            vec![
+                HashSet::from([Segment::A, Segment::B]),
+                HashSet::from([Segment::D, Segment::E, Segment::F]),
+            ]
+        )
+    }
+
+    #[test]
+    fn parse_line() {
+        let actual = Observation::parse(include_str!("tiny.txt")).unwrap().1;
+        assert_eq!(actual.patterns[9], HashSet::from([Segment::A, Segment::B]));
+    }
+
+    #[test]
+    fn parse_observation() {
+        Observation::parse_lines(include_str!("example.txt")).unwrap();
+    }
+}
