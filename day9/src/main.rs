1f9e54d4e09941547b3ba156c77a87b1dcd90cee
Adam Chalmers <adamschalmers@gmail.com>
2021-12-11 22:06:52 -0600

    Simplify, remove const generic width/height

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,7 +1,8 @@
+use cached::proc_macro::cached;
 use std::collections::HashSet;
 
 fn main() {
-    let g: Grid<u8, 100, 100> = Grid::parse(include_str!("input.txt"));
+    let g: Grid<u8> = Grid::parse(include_str!("input.txt"));
     println!("Q1: {}", g.total_low_point_risk());
 }
 
@@ -17,39 +18,47 @@ impl std::fmt::Display for Point {
     }
 }
 
-struct Grid<T, const X: usize, const Y: usize>([[T; X]; Y]);
-
-impl<T, const X: usize, const Y: usize> Default for Grid<T, X, Y>
+struct Grid<T> {
+    X: usize,
+    Y: usize,
+    cells: Vec<Vec<T>>,
+}
+impl<T> Grid<T>
 where
-    T: Default + Copy,
+    T: Default + Clone,
 {
-    fn default() -> Self {
-        Self([[T::default(); X]; Y])
+    fn new(x: usize, y: usize) -> Self {
+        Self {
+            X: x,
+            Y: y,
+            cells: vec![vec![T::default(); x]; y],
+        }
     }
 }
-
-impl<const X: usize, const Y: usize> Grid<u8, X, Y> {
+impl Grid<u8> {
     fn parse(s: &str) -> Self {
-        let mut g = Self::default();
+        let y = s.lines().count();
+        let x = s.lines().next().unwrap().len();
+        let mut g = Self::new(x, y);
         for (i, line) in s.lines().enumerate() {
             for (j, char) in line.chars().enumerate() {
-                g.0[i][j] = String::from(char).parse().unwrap();
+                g.cells[i][j] = String::from(char).parse().unwrap();
             }
         }
         g
     }
 }
 
-impl<T, const X: usize, const Y: usize> Grid<T, X, Y> {
+impl<T> Grid<T> {
     fn adjacent(&self, p: Point) -> HashSet<Point> {
         let mut out = HashSet::new();
-        if p.x < X - 1 {
+        if p.x < self.X - 1 {
             out.insert(Point { x: p.x + 1, ..p });
         }
         if p.x > 0 {
             out.insert(Point { x: p.x - 1, ..p });
         }
-        if p.y < Y - 1 {
+        if p.y < self.Y - 1 {
             out.insert(Point { y: p.y + 1, ..p });
         }
         if p.y > 0 {
@@ -58,15 +67,15 @@ impl<T, const X: usize, const Y: usize> Grid<T, X, Y> {
         out
     }
     /// Iterate over all points in this grid.
-    fn all_points(&self) -> impl Iterator<Item = Point> {
-        (0..Y).flat_map(|y| (0..X).map(move |x| Point { x, y }))
+    fn all_points(&self) -> impl Iterator<Item = Point> + '_ {
+        (0..self.Y).flat_map(|y| (0..self.X).map(move |x| Point { x, y }))
     }
     fn get(&self, p: Point) -> &T {
-        &self.0[p.y][p.x]
+        &self.cells[p.y][p.x]
     }
 }
 
-impl<T, const X: usize, const Y: usize> Grid<T, X, Y>
+impl<T> Grid<T>
 where
     T: Ord,
 {
@@ -74,13 +83,12 @@ where
         self.all_points().filter(|p| {
             self.adjacent(*p)
                 .iter()
-                // .inspect(|adj| if p.y == 4 && p.x == )
                 .all(|adj| self.get(*adj) > self.get(*p))
         })
     }
 }
 
-impl<const X: usize, const Y: usize> Grid<u8, X, Y> {
+impl Grid<u8> {
     fn total_low_point_risk(&self) -> u16 {
         self.find_low_points()
             .map(|lp| *self.get(lp) as u16 + 1)
@@ -93,31 +101,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_all_points() {
-        let g: Grid<(), 2, 2> = Default::default();
-        assert_eq!(16, g.all_points().count())
-    }
-
-    #[test]
-    fn test_adj() {
-        let g: Grid<(), 3, 3> = Default::default();
-        assert_eq!(
-            g.adjacent(Point::default()),
-            HashSet::from([Point { x: 0, y: 1 }, Point { x: 1, y: 0 }])
-        );
-        assert_eq!(
-            g.adjacent(Point { x: 2, y: 4 }),
-            HashSet::from([
-                Point { x: 1, y: 4 },
-                Point { x: 3, y: 4 },
-                Point { x: 2, y: 3 }
-            ])
-        );
-    }
-
-    #[test]
     fn q1_example() {
-        let g: Grid<u8, 10, 5> = Grid::parse(include_str!("example.txt"));
+        let g: Grid<u8> = Grid::parse(include_str!("example.txt"));
         assert_eq!(g.total_low_point_risk(), 15)
     }
 }
