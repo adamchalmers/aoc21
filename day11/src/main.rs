3d2452e9c9ab1265bd49daf3ce8334df89cafdf3
Adam Chalmers <adamschalmers@gmail.com>
2021-12-15 19:14:05 -0600

    Day 11 Part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,13 +1,17 @@
 use std::{num::ParseIntError, str::FromStr};
 
+const Q1_TURNS: usize = 100;
+
 fn main() {
     let mut g = Grid::parse(include_str!("data/input.txt"));
-    println!("Q1: {}", g.tick_n(100));
+    println!("Q1: {}", g.step_n(Q1_TURNS));
+    println!("Q2: {}", g.synchronized_at() as usize + Q1_TURNS)
 }
 
 #[derive(Debug)]
 struct Octopus {
     energy: u8,
+    /// Did it flash in this step?
     flashed: bool,
 }
 
@@ -24,6 +28,7 @@ impl FromStr for Octopus {
     }
 }
 
+#[derive(Clone, Copy)]
 struct Point {
     x: usize,
     y: usize,
@@ -40,6 +45,7 @@ where
     T: FromStr,
     <T as FromStr>::Err: core::fmt::Debug,
 {
+    /// Parse the text files from Advent of Code.
     fn parse(s: &str) -> Self {
         let cells: Vec<Vec<_>> = s
             .lines()
@@ -57,6 +63,7 @@ where
 }
 
 impl<T> Grid<T> {
+    /// Find all neighbouring points.
     fn neighbours(&self, p: Point) -> impl Iterator<Item = Point> {
         let width = self.x as isize;
         let height = self.y as isize;
@@ -79,35 +86,49 @@ impl<T> Grid<T> {
             y: y as usize,
         })
     }
-    fn get(&self, x: usize, y: usize) -> &T {
-        self.cells.get(x).unwrap().get(y).unwrap()
+
+    fn get(&self, p: Point) -> &T {
+        self.cells.get(p.x).unwrap().get(p.y).unwrap()
     }
-    fn get_mut(&mut self, x: usize, y: usize) -> &mut T {
-        self.cells.get_mut(x).unwrap().get_mut(y).unwrap()
+
+    fn get_mut(&mut self, p: Point) -> &mut T {
+        self.cells.get_mut(p.x).unwrap().get_mut(p.y).unwrap()
+    }
+
+    /// Iterate over all points in the grid.
+    fn points(&self) -> impl Iterator<Item = Point> {
+        let height = self.y;
+        let width = self.x;
+        (0..height).flat_map(move |y| (0..width).map(move |x| Point { x, y }))
     }
 }
 
 impl Grid<Octopus> {
-    fn tick(&mut self) -> u16 {
-        for x in 0..self.x {
-            for y in 0..self.y {
-                self.get_mut(x, y).flashed = false;
-                self.get_mut(x, y).energy += 1;
-            }
+    /// Let n steps of time pass. Octopuses increase their energy and might flash.
+    fn step_n(&mut self, n: usize) -> u16 {
+        (0..n).fold(0, |num_flashes, _| num_flashes + self.step())
+    }
+
+    /// Let one step of time pass. Octopuses increase their energy and might flash.
+    fn step(&mut self) -> u16 {
+        // Each octopus gains energy.
+        for p in self.points() {
+            self.get_mut(p).flashed = false;
+            self.get_mut(p).energy += 1;
         }
+
+        // If any octopus flashes, its neighbours gain energy, possibly flashing themselves.
         let mut num_flashes = 0;
         loop {
             let mut something_flashed = false;
 
-            for x in 0..self.x {
-                for y in 0..self.y {
-                    if self.get(x, y).energy > 9 && !self.get(x, y).flashed {
-                        self.get_mut(x, y).flashed = true;
-                        num_flashes += 1;
-                        something_flashed = true;
-                        for p in self.neighbours(Point { x, y }) {
-                            self.get_mut(p.x, p.y).energy += 1;
-                        }
+            for p in self.points() {
+                if self.get(p).energy > 9 && !self.get(p).flashed {
+                    self.get_mut(p).flashed = true;
+                    num_flashes += 1;
+                    something_flashed = true;
+                    for p in self.neighbours(p) {
+                        self.get_mut(p).energy += 1;
                     }
                 }
             }
@@ -117,17 +138,32 @@ impl Grid<Octopus> {
             }
         }
 
-        for x in 0..self.x {
-            for y in 0..self.y {
-                if self.get(x, y).flashed {
-                    self.get_mut(x, y).energy = 0;
-                }
+        // Reset the energy of any octopus that flashed in this step.
+        for p in self.points() {
+            if self.get(p).flashed {
+                self.get_mut(p).energy = 0;
             }
         }
         num_flashes
     }
-    fn tick_n(&mut self, n: usize) -> u16 {
-        (0..n).fold(0, |num_flashes, _| num_flashes + self.tick())
+
+    /// Did every octopus flash in the previous step?
+    fn all_flashed(&self) -> bool {
+        self.points()
+            .map(|p| self.get(p))
+            .all(|octopus| octopus.flashed)
+    }
+
+    /// After how many steps will each octopus flash simultaneously?
+    fn synchronized_at(&mut self) -> u16 {
+        let mut i = 1;
+        loop {
+            self.step();
+            if self.all_flashed() {
+                return i;
+            }
+            i += 1;
+        }
     }
 }
 
@@ -144,63 +180,36 @@ mod tests {
     }
 
     #[test]
-    fn test_one_flash() {
-        let s = r#"11111
-19991
-19191
-19991
-11111"#;
-        let mut g = Grid::<Octopus>::parse(s);
-        assert_eq!(g.tick(), 9);
-        assert_eq!(g.tick(), 0);
-    }
-
-    impl std::fmt::Debug for Grid<Octopus> {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            f.debug_struct("Grid")
-                .field("x", &self.x)
-                .field("y", &self.y)
-                .finish()
-        }
-    }
-
-    impl PartialEq for Octopus {
-        fn eq(&self, other: &Self) -> bool {
-            self.energy == other.energy
-        }
-    }
-
-    impl Eq for Octopus {}
-
-    impl Grid<Octopus> {
-        fn print_cells(&self) {
-            for y in 0..self.y {
-                let mut line = String::new();
-                for x in 0..self.x {
-                    let ch = self.cells[x][y].energy.to_string().chars().next().unwrap();
-                    line.push(ch)
-                }
-                println!("{}", line);
-            }
-        }
-    }
-
-    #[test]
     fn test_one_flash_example() {
         let mut g = Grid::<Octopus>::parse(include_str!("data/example.txt"));
-        g.tick();
+        g.step();
         let expected = Grid::<Octopus>::parse(include_str!("data/example_step2.txt"));
-        g.print_cells();
-        println!("\n");
-        expected.print_cells();
-        assert_eq!(g.cells, expected.cells);
+        assert_eq!(
+            g.cells
+                .into_iter()
+                .map(|row| row.iter().map(|oct| oct.energy).collect::<Vec<_>>())
+                .collect::<Vec<_>>(),
+            expected
+                .cells
+                .into_iter()
+                .map(|row| row.iter().map(|oct| oct.energy).collect::<Vec<_>>())
+                .collect::<Vec<_>>()
+        );
     }
 
     #[test]
     fn test_example_q1() {
         let mut g = Grid::<Octopus>::parse(include_str!("data/example.txt"));
         let expected = 1656;
-        let actual = g.tick_n(100);
+        let actual = g.step_n(100);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_example_q2() {
+        let mut g = Grid::<Octopus>::parse(include_str!("data/example.txt"));
+        let expected = 195;
+        let actual = g.synchronized_at();
         assert_eq!(actual, expected);
     }
 }
